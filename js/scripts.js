0ac83243f7a1805af99036671fed74507eeead01
AleksVZ <zaiko1984@ukr.net>
2022-09-05 18:46:45 +0300

    add owl - part 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ $(document).ready(function() {
 		e.stopPropagation();
 	});
 });
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        nav: false,
+        dots:false,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            1030:{
+                items:1
+            }
+        }
+    });
+
+});
