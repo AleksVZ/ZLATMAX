1df8d3c60523c0fc190e3d68558a837f0e686f4a
AleksVZ <zaiko1984@ukr.net>
2022-09-29 12:32:31 +0300

    add price slider

--- js/scripts.js
+++ js/scripts.js
@@ -136,3 +136,18 @@ $(document).ready(function(){
         }
     });
 });
+
+//! ______________
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 5000,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val(ui.values[ 0 ] + "  руб." + "     -     " + ui.values[ 1 ] + "  руб." );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + "  руб." + "     -     " + 
+       $( "#slider-range" ).slider( "values", 1 ) + "  руб." );
+  } );
