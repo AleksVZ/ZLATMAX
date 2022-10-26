a941dffc450a8f1d3378b658ae1fef8b8d74e7c9
AleksVZ <zaiko1984@ukr.net>
2022-10-26 16:24:44 +0300

    add section (card_product) form

--- js/scripts.js
+++ js/scripts.js
@@ -311,3 +311,30 @@ $("#slider-container_blade").slider({
     console.log( from + " --- " + to );
     }
 });
+
+// слайдер галереи карточки товара
+const container = document.querySelector('.container_big_photo');
+const preview = document.querySelector('.prev')
+const active = document.querySelectorAll('.thumb');
+
+
+container.addEventListener('click', function(e){
+  
+  if( e.target.className == 'thumb' ){
+    preview.src= e.target.src;
+    preview.classList.add('effect');
+    
+    setTimeout(function(){
+      preview.classList.remove('effect');
+    }, 100)
+    
+    active.forEach(function(thumb){
+      if (thumb.classList.contains('active')){
+        thumb.classList.remove('active');
+      }
+    });
+    
+    e.target.classList.add('active');
+  }
+  
+});
