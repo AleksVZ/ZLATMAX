602a5c20c2c05838b00ffc95eb979b88cddd2527
AleksVZ <zaiko1984@ukr.net>
2022-11-01 21:01:43 +0200

    add tooltip(section price_knife)

--- js/scripts.js
+++ js/scripts.js
@@ -312,7 +312,7 @@ $("#slider-container_blade").slider({
     }
 });
 
-// слайдер галереи карточки товара
+// слайдер галереи карточки товара(замена фотографий)
 const container = document.querySelector('.container_big_photo');
 const preview = document.querySelector('.prev')
 const active = document.querySelectorAll('.thumb');
@@ -338,3 +338,27 @@ container.addEventListener('click', function(e){
   }
   
 });
+
+
+
+// billing box
+function updateCounter(idCounter, idCost, idTotal, increment) {
+	var counter = document.getElementById(idCounter);
+	var amount = 0;
+	if (!isNaN(counter.value) && counter.value >= 0) {
+		amount = Number(counter.value) + increment;
+		if (amount < 0) {
+			amount = 0;
+		}
+	} else {
+		amount = 0;
+	}
+	counter.value = amount;
+	costPerPiece = Number(document.getElementById(idCost).innerText);
+	document.getElementById(idTotal).innerText = (amount * costPerPiece).toFixed(2);
+	updatePrices();
+}
+// ___________
+
+
+
